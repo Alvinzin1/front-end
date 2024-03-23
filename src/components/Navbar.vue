392faf7c4cf2460690b62d6a5168bfa50c7a3e7e
OliveiraGabrielDev <gabrielsouzacontato.99@gmail.com>
2024-03-23 12:58:43 -0300

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <header class="text-gray-600 body-font">
+    <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+        <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+        <span class="ml-3 text-xl">Desafio - Mercadinhos</span>
+        </a>
+        <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <router-link to="/" class="mr-5 hover:text-gray-900" >Inicio</router-link>
+            <router-link to="/about" class="mr-5 hover:text-gray-900" >Produtos</router-link>
+        </nav>
+        <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 mx-1 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Login
+        </button>
+        <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 mx-1 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+            Cadastre-se
+        </button>
+    </div>
+    </header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,45 @@
+<template>
+    <!-- Seção de produtos mais baratos -->
+    <section class="text-gray-600 body-font">
+        <div class="container px-5 py-24 mx-auto">
+            <h1 class="font-bold text-3xl mb-4">Barato do dia</h1>
+            <div class="flex flex-wrap -m-4">
+                <ItemCard v-for="(item, index) in items" :key="item.id" :data="item"></ItemCard>
+            </div>
+        </div>
+    </section>
+</template>
+
+<script>
+import ItemCard from '@/components/ItemCard.vue';
+
+export default{
+    name: "HomeView",
+    components: {
+        ItemCard
+    },
+    data() {
+        return {
+            items: [
+                {
+                    name: 'produto 1',
+                    price: '9,32',
+                    store: 'Mercado 1'
+                },{
+                    name: 'produto 2',
+                    price: '19,52',
+                    store: 'Mercado 2'
+                },{
+                    name: 'produto 3',
+                    price: '5,22',
+                    store: 'Mercado 3'
+                },{
+                    name: 'produto 4',
+                    price: '6,01',
+                    store: 'Mercado 4'
+                }
+            ]
+        }
+    },
+}
+</script>
